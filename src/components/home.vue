f68f1687aa6c48716a8972788a1dec20902d3bb3
DzLuo <zdthm2010@gmail.com>
2020-06-05 16:58:23 +0800

    侧边栏完成

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,22 +1,136 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <!--头部区域-->
+  <el-container class="home-container">
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt="">
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!--    页面主体区域-->
+    <el-container>
+      <!--      侧边栏-->
+      <el-aside :width="isCollapse? '64px':'200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <!--侧边栏菜单区域-->
+        <el-menu
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          :unique-opened="true"
+          background-color="#313743"
+          text-color="#fff"
+          active-text-color="#409BFF">
+          <!--一级菜单-->
+          <el-submenu :index="item.id+''" :key="item.id" v-for="item in menuList">
+            <!--一级菜单的模板区-->
+            <template slot="title">
+              <i :class="iconsObj[item.id]"></i>
+              <span>{{item.authName}}</span>
+            </template>
+            <!--二级菜单-->
+            <el-menu-item :index="subItem.id + ''" :key="subItem.id" v-for="subItem in item.children">
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!--右侧内容区-->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 
 </template>
 
 <script>
 export default {
   name: 'home',
+  data () {
+    return {
+      menuList: [],
+      iconsObj: {
+        125: 'iconfont icon-user',
+        103: 'iconfont icon-tijikongjian',
+        101: 'iconfont icon-shangpin',
+        102: 'iconfont icon-danju',
+        145: 'iconfont icon-baobiao'
+      },
+      // 是否折叠
+      isCollapse: false
+    }
+  },
+  created () {
+    this.getMenuList()
+  },
   methods: {
     logout () {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    async getMenuList () {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.mate.msg)
+      this.menuList = res.data
+    },
+    // 点击按钮切换折叠展开
+    toggleCollapse () {
+      this.isCollapse = !this.isCollapse
     }
   }
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+  .home-container {
+    height: 100%;
+  }
 
+  .el-header {
+    background-color: #363d40;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+
+    > div {
+      display: flex;
+      align-items: center;
+
+      span {
+        margin-left: 15px;
+      }
+    }
+
+  }
+
+  .el-aside {
+    background-color: #313743;
+
+    .el-menu {
+      border-right: none;
+    }
+  }
+
+  .el-main {
+    background-color: #e9edf1;
+  }
+
+  .iconfont {
+    margin-right: 10px;
+  }
+
+  .toggle-button {
+    background-color: #313743;
+    font-size: 10px;
+    line-height: 24px;
+    color: #fff;
+    text-align: center;
+    letter-spacing: 0.2em;
+    cursor: pointer;
+  }
 </style>
