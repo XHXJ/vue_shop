4cabb5b6548bcc4ea45f14a169d75d2ae53ec12b
DzLuo <zdthm2010@gmail.com>
2020-06-18 17:03:15 +0800

    角色权限分配完成

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -16,16 +16,47 @@
       <!--      角色列表区域-->
       <el-table :data="rolesList" border stripe>
         <!--        展开列-->
-        <el-table-column type="expand"></el-table-column>
+        <el-table-column type="expand">
+          <template slot-scope="scope">
+            <el-row :class="['bdbottom',i1 ===0 ? 'bdtop': '','vcenter']" :key="item1.id"
+                    v-for="(item1,i1 ) in scope.row.children">
+              <!--              渲染一级权限-->
+              <el-col :span="5">
+                <el-tag closable @close="removeRightById(scope.row,item1.id)">{{item1.authName}}</el-tag>
+                <i class="el-icon-caret-right"></i>
+              </el-col>
+              <!--              渲染二级和三级权限-->
+              <el-col :span="19">
+                <el-row :class="['bdtop',i2 ===0 ? '':'bdtop','vcenter']" v-for="(item2,i2) in item1.children"
+                        :key="item2.id">
+                  <el-col :span="6">
+                    <el-tag closable @close="removeRightById(scope.row,item2.id)" type="success">{{item2.authName}}
+                    </el-tag>
+                    <i class="el-icon-caret-right"></i>
+                  </el-col>
+                  <el-col :span="18">
+                    <el-tag closable @close="removeRightById(scope.row,item3.id)" type="warning"
+                            v-for="(item3) in item2.children"
+                            :key="item3.id">
+                      {{item3.authName}}
+                    </el-tag>
+                  </el-col>
+                </el-row>
+              </el-col>
+            </el-row>
+          </template>
+        </el-table-column>
         <el-table-column type="index"></el-table-column>
         <el-table-column label="角色名称" prop="roleName"></el-table-column>
         <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
         <el-table-column label="操作" width="300px">
           <template slot-scope="scope">
-            <el-button size="mini" type="primary" icon="el-icon-edit" @click="showEditDialog(scope.row.id)">编辑</el-button>
+            <el-button size="mini" type="primary" icon="el-icon-edit" @click="showEditDialog(scope.row.id)">编辑
+            </el-button>
             <el-button size="mini" type="danger" icon="el-icon-delete" @click="removeRolesById(scope.row.id)">删除
             </el-button>
-            <el-button size="mini" type="warning" icon="el-icon-setting">分配权限</el-button>
+            <el-button size="mini" type="warning" icon="el-icon-setting" @click="showSetRightDialog(scope.row)">分配权限
+            </el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -70,8 +101,29 @@
       </el-form>
       <!--      底部区域-->
       <span slot="footer" class="dialog-footer">
-    <el-button @click="addDialogVisible = false">取 消</el-button>
+    <el-button @click="editDialogVisible = false">取 消</el-button>
     <el-button type="primary" @click="editRolesInfo">保存</el-button>
+  </span>
+    </el-dialog>
+    <!--    分配权限的对话框-->
+    <el-dialog
+      title="分配权限"
+      :visible.sync="setRightDialogVisible"
+      width="50%"
+      @close="setRightDialogClosed"
+    >
+      <!--      树形控件-->
+      <el-tree :data="rightsList" :props="treeProps"
+               show-checkbox
+               default-expand-all
+               :default-checked-keys="defKeys"
+               ref="treeRef"
+               node-key="id"></el-tree>
+
+      <!--      底部区域-->
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="setRightDialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="allotRights">保存</el-button>
   </span>
     </el-dialog>
   </div>
@@ -106,7 +158,20 @@ export default {
         ]
       },
       editDialogVisible: false,
-      editForm: {}
+      editForm: {},
+      setRightDialogVisible: false,
+
+      // 所有权限数据
+      rightsList: [],
+      // 树形控件的属性绑定对象
+      treeProps: {
+        label: 'authName',
+        children: 'children'
+      },
+      // 默认选中的节点id
+      defKeys: [],
+      // 当前即将分配权限的角色id
+      roleId: ''
     }
   },
   methods: {
@@ -162,6 +227,51 @@ export default {
         this.$message.success('更新角色信息成功')
         await this.getRolesList()
       })
+    },
+    // 根据id删除对应权限
+    async removeRightById (role, rightId) {
+      const confirmResult = await this.$confirm('此操作将永久删除该权限, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err)
+      if (confirmResult !== 'confirm') return this.$message.info('已取消删除操作')
+      const { data: res } = await this.$http.delete(`roles/${role.id}/rights/${rightId}`)
+      if (res.meta.status !== 200) return this.$message.error('删除权限失败')
+      this.$message.success('删除权限成功')
+      role.children = res.data
+    },
+    // 展示分配权限的对话框
+    async showSetRightDialog (role) {
+      // 保存当前选中的角色id给后面的提交用
+      this.roleId = role.id
+      const { data: res } = await this.$http.get('rights/tree')
+      if (res.meta.status !== 200) return this.$message.error('获取权限数据失败')
+      this.rightsList = res.data
+      // 递归获取三级节点的id
+      this.getLeafKeys(role, this.defKeys)
+      this.setRightDialogVisible = true
+    },
+    setRightDialogClosed () {
+      this.defKeys = []
+    },
+    // 通过递归的形式获取角色三级权限列表的id
+    getLeafKeys (node, arr) {
+      // 如果当前node节点不包含children则是三级节点
+      if (!node.children) return arr.push(node.id)
+      node.children.forEach(item => {
+        this.getLeafKeys(item, arr)
+      })
+    },
+    // 点击为角色分配权限
+    async allotRights () {
+      const keys = [...this.$refs.treeRef.getCheckedKeys(), ...this.$refs.treeRef.getHalfCheckedKeys()]
+      const idStr = keys.join(',')
+      const { data: res } = await this.$http.post(`roles/${this.roleId}/rights`, { rids: idStr })
+      if (res.meta.status !== 200) return this.$message.error('分配角色权限失败!')
+      this.$message.success('分配权限成功!')
+      await this.getRolesList()
+      this.setRightDialogVisible = false
     }
   },
   created () {
@@ -171,5 +281,20 @@ export default {
 </script>
 
 <style scoped>
+  .el-tag {
+    margin: 7px;
+  }
 
+  .bdtop {
+    border-top: 1px solid #eeeeee;
+  }
+
+  .bdbottom {
+    border-bottom: 1px solid #eeeeee;
+  }
+
+  .vcenter {
+    display: flex;
+    align-items: center;
+  }
 </style>
